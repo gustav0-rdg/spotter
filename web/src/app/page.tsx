dd4e2e86ad57f3a38a224d8991659e219a75e326
gustavo rodrigues <gustavolourenco821@gmail.com>
2025-12-18 10:54:16 -0300

    ADD: Base pra landing page do projeto web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,65 +1,23 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+export default function Home(){
+  return(
+    <main className="spotter h-screen w-screen flex flex-col md:w-[768px] md:h-screen md:flex md:flex-col md:items-center m-auto">
+      <header className="spotter__header  w-full h-20 px-8 flex items-center gap-16 justify-around">
+        <h4 className="ml-2 text-2xl font-semibold tracking-wide">Spotter</h4>
+        <button
+          className="bg-[var(--btn-bg)] rounded-full w-35/100 h-35/100 md:w-20/100 md:h-50/100"
+        >
+          Entrar</button>
+      </header>
+      <main className="w-full p-4 flex flex-col justify-center items-center gap-4">
+        <h1 className="text-4xl font-bold text-center">Seu personal trainer de bolso</h1>
+        <div className="rounded-xl bg-[var(--card-bg)] p-6 h-auto shadow-lg">
+            <p className="">Não adivinhe mais pesos. Spotter conecta você com um treinador profissional e usa predições com IA para ajustar cargas e exercicíos com base na sua perfomance.</p>
+
         </div>
       </main>
-    </div>
-  );
+      
+    </main>
+  )
 }
